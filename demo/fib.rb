f13912c9dd682434c6f87467d518092525520b3a
Arthur Neves <arthurnn@gmail.com>
2013-09-19 19:54:20 -0400

    new fib to use server

--- demo/fib.rb
+++ demo/fib.rb
@@ -1,12 +1,20 @@
+require 'byebug'
+
+p Byebug::VERSION
+
+Byebug.wait_connection = true
+Byebug.start_websocket_server
+
 class Fixnum
-	def fibonacci
-		if (self < 3)  then
-			byebug
-			return 1
-		else
-			(self - 1).fibonacci + (self - 2).fibonacci
-		end
-	end
+  def fibonacci
+    if (self < 3)  then
+#      byebug
+      return 1
+    else
+      (self - 1).fibonacci + (self - 2).fibonacci
+    end
+  end
 end
 
+byebug
 12.fibonacci
