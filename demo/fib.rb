3fb7cad84905df5ce406cf7fd699beaa221d399f
Arthur Neves <arthurnn@gmail.com>
2013-09-20 15:23:03 -0400

    new demo

--- demo/fib.rb
+++ demo/fib.rb
@@ -4,11 +4,20 @@ p Byebug::VERSION
 Byebug.start_websocket_server(browser: true)
 
 class Fixnum
+
   def fibonacci
-    if (self < 3)  then
-#      byebug
+    if (self < 3)
       return 1
     else
+      abc = "arthurnn"
+      c = Class.new do
+        def foo
+          @in = 1
+          @out = 111
+        end
+      end
+      c.new.foo
+
       (self - 1).fibonacci + (self - 2).fibonacci
     end
   end
